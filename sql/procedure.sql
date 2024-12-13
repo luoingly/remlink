43e94e8f2371b66d62e4ca7f6b14ca501789b493
luoingly <signature@luoingly.top>
2024-12-13 15:52:12 +0800

    fix: posts fetch privacy logic

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -7,13 +7,12 @@ CREATE PROCEDURE IF NOT EXISTS get_visible_posts(
 BEGIN
     SELECT DISTINCT p.*, 
            CASE WHEN l.user_id IS NOT NULL THEN 1 ELSE 0 END AS liked
-    FROM posts_view pv
+    FROM posts_view p
     LEFT JOIN follows f1 ON p.user_id = f1.followee_id
     LEFT JOIN follows f2 ON p.user_id = f2.follower_id
     LEFT JOIN likes l ON p.post_id = l.post_id AND l.user_id = viewer_id
     WHERE (target_user_id IS NULL OR p.user_id = target_user_id) AND (
-        p.privacy = 'public'
-        OR (p.privacy = 'private' AND p.user_id = viewer_id)
+        p.privacy = 'public' OR p.user_id = viewer_id
         OR (p.privacy = 'friends' AND f1.follower_id = viewer_id AND f2.followee_id = viewer_id))
     ORDER BY p.created_at DESC
     LIMIT `offset`, `limit`;
@@ -27,13 +26,12 @@ CREATE PROCEDURE IF NOT EXISTS get_post(
 BEGIN
     SELECT p.*, 
            CASE WHEN l.user_id IS NOT NULL THEN 1 ELSE 0 END AS liked
-    FROM posts_view pv
+    FROM posts_view p
     LEFT JOIN follows f1 ON p.user_id = f1.followee_id
     LEFT JOIN follows f2 ON p.user_id = f2.follower_id
     LEFT JOIN likes l ON p.post_id = l.post_id AND l.user_id = viewer_id
     WHERE p.post_id = post_id AND (
-        p.privacy = 'public'
-        OR (p.privacy = 'private' AND p.user_id = viewer_id)
+        p.privacy = 'public' OR p.user_id = viewer_id
         OR (p.privacy = 'friends' AND f1.follower_id = viewer_id AND f2.followee_id = viewer_id))
     LIMIT 1;
 END;
